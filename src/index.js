ffaaa33caf90e4a3172c009eebee518071507574
gour9808 <gouravsharma614@gmail.com>
2023-09-25 19:24:36 +0530

    remove

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
 const userRoutes = require("./routes/users");
-const masterRoutes = require("./routes/master-data");
-const expensesDataRouter = require("./routes/expense");
 const dealDataRouter = require("./routes/deals");
 
 
@@ -13,8 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(userRoutes);
-app.use(masterRoutes);
-app.use(expensesDataRouter);
 app.use(dealDataRouter);
 
 
